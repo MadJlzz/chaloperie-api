724306bbeeffffe37848cfda16b2ff0a46887a4a
Julien KLAER <klaer.julien@gmail.com>
2021-02-06 22:21:31 +0100

    Adding more testing to the writing function.

--- functions/write/write_test.go
+++ functions/write/write_test.go
@@ -1,10 +1,41 @@
 package write
 
 import (
+	"bufio"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCatHTTPContentKO(t *testing.T) {
+	r := httptest.NewRequest("POST", "/", strings.NewReader("This is only plain text."))
+	r.Header.Add("Content-Type", "text/plain")
+
+	rr := httptest.NewRecorder()
+	CatHTTP(rr, r)
+
+	resp := rr.Result()
+	respBody := bufio.NewScanner(resp.Body)
+	respBody.Scan()
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	assert.Equal(t, "Something wrong happend and your cat could not be created 😿", respBody.Text())
+}
+
+func TestCatHTTPContentOK(t *testing.T) {
+	r := httptest.NewRequest("POST", "/", strings.NewReader(`{"title": "Shadow does HAI", "pictureUrl": "https://google.com"}`))
+	r.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	CatHTTP(rr, r)
+
+	resp := rr.Result()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
 
 func TestValidateInputsTitleToSmall(t *testing.T) {
 	c := catCreateRequest{}
@@ -23,5 +54,15 @@ func TestValidateInputsTitleToBig(t *testing.T) {
 		t.Error("empty pictureURL should have thrown an error")
 	} else {
 		assert.Equal(t, err.Error(), "A cat image should be provided 😼")
+	}
+}
+
+func TestValidateInputsOk(t *testing.T) {
+	c := catCreateRequest{
+		Title:      "Shadow does HAI",
+		PictureURL: "https://my.image.com",
+	}
+	if err := validateInputs(c); err != nil {
+		t.Error("validateInputs should return no error")
 	}
 }
